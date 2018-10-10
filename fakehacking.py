697e8f56e8804ab7934c84ae7fac7da17cb436f0
Hans0ddcraft <43818889+Hans0ddcraft@users.noreply.github.com>
2018-10-09 20:25:53 -0500

    Added "fakehacking"

---
+++ fakehacking.py
@@ -0,0 +1,15 @@
+#Trick friends by running this code that makes it look like you're hacking!
+
+x=0
+H = ['Intel Chip','Transfering $9,127,182,192, to your bank account','The crown is at -1.2093,9.2039','Time to fight','Username = admin','Downloading database ####################################################Passwords located at: C:/(user)/Passwords','Checking Systems','Sending payload','Cracking Passwords','Launching Nukes','running Hashcat','Checking tutorials', 'Checking confidential files', 'Searching Darkweb','Starting Phase 1', 'Intiating', 'Phase 2 is go', 'Password = 08/20/2020', 'Covering tracks','Checking for 404', 'Ah, Superman!', 'Downloading.........................................Done']
+from random import shuffle
+from random import random
+import time
+while True:
+  for letter in range(0,100):
+    print(H[0])
+    shuffle(H)
+    time.sleep(0.5)
+    if x != 100:
+     print(random())
+     time.sleep(0.5)
